da3f39be258294a772e12ca7b5f0be8cb07c9024
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-02-17 23:57:56 +0530

    hero section

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Hero = () => {
     return (
@@ -9,7 +10,11 @@ const Hero = () => {
                     <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-black font-bold uppercase'>
                         mega sale <span className='text-rose-600'>Special</span> Offer up to  <span className='text-orange-500'>60%</span> off
                     </h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptatum molestiae, veniam laborum reiciendis at vitae illo eius aliquid quas natus odio! Odit eum nesciunt vel consequatur corporis non libero?</p>
+                    <p className='text-sm md:text-base lg:text-lg text-black text-opacity-70 mt-4'>Don't miss out on our biggest sale of the season! Grab your favorite products at unbeatable prices and enjoy massive discounts of up to 60% off. Shop now while stocks last!</p>
+                    <div className='flex items-center space-x-4 mt-6'>
+                   <Button size={'lg'} className='bg-blue-700'>Shop Now</Button>
+                   <Button size={'lg'} >Explor More</Button>
+                    </div>
                 </div>
 
             </div>
