cb621c89d0e564954e7fb2e00b4ffd8007c9080c
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-02-19 23:33:35 +0530

    product card done

--- components/Home/ProductCard.tsx
+++ components/Home/ProductCard.tsx
@@ -1,7 +1,10 @@
 "use client"
 import { Product } from '@/typing'
+import { Heart, ShoppingBag, StarIcon } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
 
 type Props = {
     product: Product
@@ -12,9 +15,33 @@ const ProductCard = ({ product }: Props) => {
     const num = Math.round(product.rating.rate)
     const ratingArray = new Array(num).fill(0)
     return (
-        <div className='w-[200px] h-[150px]'>
-            <Image src={product.image} alt={product.title} width={100} height={100} className='rounded-lg w-[80%] h-[80%] object-contain' />
+        <div className='p-4'>
+            <div className='w-[200px] h-[150px]'>
+                <Image src={product.image} alt={product.title} width={100} height={100} className='rounded-lg w-[80%] h-[80%] object-contain' />
+            </div>
+            <p className='mt-5 text-xs capitalize text-gray-600'>{product.category}</p>
+            <Link href={`/product/product-details/${product.id}`}>
+                <h1 className='mt-2 text-lg cursor-pointer hover:text-blue-900 transition-all  hover:underline  sm:w-full sm:truncate font-semibold text-black'>{product.title}</h1>
+            </Link>
+            <div className='flex items-center '>
+                {ratingArray.map((star) => {
+                    return <StarIcon key={Math.random()*1000} size={16} fill='yellow' className='text-yellow-500' />
+                })}
+            </div>
+            <div className='flex mt-2 items-center  space-x-2'>
+                <p className='text-black text-base line-through font-semibold opacity-50'>{`₹${(product.price + 10).toFixed(2)}`}</p>
+                <p className='text-black text-lg opacity-80 font-bold'>{`₹${product.price.toFixed(2)}`}</p>
+            </div>
+            <div className='mt-4 flex items-center space-x-2'>
+                <Button size={"icon"}>
+                    <ShoppingBag size={18}/>
+                </Button>
+                <Button size={"icon"} className='bg-red-500 text-white'>
+                    <Heart size={18}/>
+                </Button>
+            </div>
         </div>
+
     )
 }
 
